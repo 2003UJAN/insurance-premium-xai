349db0b580703e2ade99377f69371ac1928db62b
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2026-01-04 02:00:50 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Explainable Insurance Premium System",
+    layout="wide"
+)
+
+st.title("Explainable AI for Insurance Premium Prediction")
+st.markdown("""
+This application demonstrates:
+- Exploratory Data Analysis  
+- City-wise premium heatmaps using OpenStreetMap  
+- Explainable AI (SHAP) for premium transparency  
+""")
+
+st.sidebar.success("Select a page from above 👆")
 
