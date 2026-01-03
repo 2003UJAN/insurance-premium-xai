6e4c29a9a8f5aedd4eccd83a8da2cc1784df8eb8
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2026-01-04 02:09:09 +0530

    Update load_data.py

--- streamlit_app/utils/load_data.py
+++ streamlit_app/utils/load_data.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 @st.cache_data
 def load_dataset():
-    return pd.read_csv("../data/insurance_premium_dataset_50000_records.csv")
+    return pd.read_csv("/data/insurance_premium_dataset_50000_records.csv")
 
