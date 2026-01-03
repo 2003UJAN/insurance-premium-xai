efce50bf348bf44fa276f7ae16ada29eae15e46e
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2026-01-04 02:02:00 +0530

    Update map_utils.py

--- streamlit_app/utils/map_utils.py
+++ streamlit_app/utils/map_utils.py
@@ -1,1 +1,27 @@
+import folium
+
+def create_city_map(city_df):
+    lat = city_df["latitude"].mean()
+    lon = city_df["longitude"].mean()
+
+    m = folium.Map(
+        location=[lat, lon],
+        zoom_start=11,
+        tiles="OpenStreetMap"
+    )
+
+    for _, row in city_df.iterrows():
+        folium.CircleMarker(
+            location=[row["latitude"], row["longitude"]],
+            radius=6,
+            popup=f"""
+            <b>{row['locality_name']}</b><br>
+            Premium: ₹{int(row['insurance_premium'])}
+            """,
+            color="red",
+            fill=True,
+            fill_opacity=0.7
+        ).add_to(m)
+
+    return m
 
