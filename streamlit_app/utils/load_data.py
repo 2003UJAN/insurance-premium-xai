752f5ad0fd306ba829cb2a7eb9f8a83c3d17ceb0
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2026-01-04 10:19:17 +0530

    Update load_data.py

--- streamlit_app/utils/load_data.py
+++ streamlit_app/utils/load_data.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import streamlit as st
+import os
+
+@st.cache_data
+def load_dataset():
+    # Resolve project root safely
+    BASE_DIR = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "..")
+    )
+
+    data_path = os.path.join(
+        BASE_DIR,
+        "data",
+        "insurance_premium_dataset_50000_records.csv"
+    )
+
+    return pd.read_csv(data_path)
 
