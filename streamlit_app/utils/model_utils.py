8c92f01845cf022824602047f389af23dfc7dcc9
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2026-01-04 12:03:33 +0530

    Update model_utils.py

--- streamlit_app/utils/model_utils.py
+++ streamlit_app/utils/model_utils.py
@@ -1,18 +1,33 @@
 import joblib
-import pandas as pd
+import os
 from sklearn.preprocessing import LabelEncoder
 
 def load_model():
-    return joblib.load("/models/premium_model.pkl")
+    BASE_DIR = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..")
+    )
+
+    model_path = os.path.join(
+        BASE_DIR,
+        "models",
+        "premium_model.pkl"
+    )
+
+    return joblib.load(model_path)
 
 def preprocess(df):
     df = df.drop(columns=["locality_name"])
 
     categorical_cols = [
-        "city_name","locality_type","terrain_type",
-        "natural_disaster_risk","urban_flood_risk",
-        "gender","occupation_risk_level",
-        "alcohol_consumption","physical_activity_level",
+        "city_name",
+        "locality_type",
+        "terrain_type",
+        "natural_disaster_risk",
+        "urban_flood_risk",
+        "gender",
+        "occupation_risk_level",
+        "alcohol_consumption",
+        "physical_activity_level",
         "health_checkup_frequency"
     ]
 
@@ -20,4 +35,3 @@ def preprocess(df):
         df[col] = LabelEncoder().fit_transform(df[col])
 
     return df
-
